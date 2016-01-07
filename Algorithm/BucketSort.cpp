fd33443cd82639730a41087d097b14cd4396c82c
wangxiong1 <wxwangxiong@gmail.com>
2016-01-07 09:39:10 +0800

    Bucket sort
    2st

--- Algorithm/BucketSort.cpp
+++ Algorithm/BucketSort.cpp
@@ -2,23 +2,30 @@
 int main()
 {
     int a[11],i,j,t;
-    for(i=0;i<=10;i++)
+    for(i=0;i<11;i++)
         a[i]=0;
 
-    for(i=1;i<=5;i++)
-    {
-        scanf("%d", &t);
+    for(i=0;i<5;i++){
+        scanf("%d",&t);
         a[t]++;
     }
 
     for(i=0;i<11;i++){
         for(j=0;j<a[i];j++){
-            printf("%d",i);
+            printf("%d ",i);
         }
     }
 
-    getchar();
-    getchar();
+    printf("\n");
 
+    for(i=10;i>=0;i--){
+        for(j=0;j<a[i];j++){
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+
+    getchar();
     return 0;
 }
