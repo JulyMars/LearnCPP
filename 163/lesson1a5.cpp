f78c6f3c4eee093c8849c3944ddc5c8d9240f24c
wangxiong <wxwangxiong@gmail.com>
2016-04-10 20:45:24 +0800

    Add:Lesson 1.5 V3

--- 163/lesson1a5.cpp
+++ 163/lesson1a5.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+	//cin,以空格、tab、回车作为分隔符
+	//cin.getline,以回车作为分隔符
 	char name1[50],name2[50];
-	cin>>name1>>name2;
+	cin.getline(name1, 50);
+	cin.getline(name2, 50);
 	cout<< "######################"<<endl;
 	cout<< name1 <<endl;
 	cout<<endl;
