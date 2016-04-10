f9feecfae156a47acf1be1b5f59a4bc47091cd29
wangxiong <wxwangxiong@gmail.com>
2016-04-10 20:31:16 +0800

    Add:Lesson1.5 V2

--- 163/lesson1a5.cpp
+++ 163/lesson1a5.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+	char name1[50],name2[50];
+	cin>>name1>>name2;
 	cout<< "######################"<<endl;
-	cout<< "Zhang" <<endl;
+	cout<< name1 <<endl;
 	cout<<endl;
 	cout<< "   Happy birthday to you!" <<endl;
 	cout<<endl;
-	cout<< "      sincerely yours Wang" <<endl;
+	cout<< "      " << name2 <<endl;
 	cout<< "######################" <<endl;	return 0;
 }
