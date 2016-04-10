9b8591b6b6a303f3477a667af526a86ca1d6a73b
wangxiong <wxwangxiong@gmail.com>
2016-04-10 21:46:52 +0800

    Add: Lesson 1.7 V3

--- 163/lesson1d7.cpp
+++ 163/lesson1d7.cpp
@@ -4,9 +4,11 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	double sum,money,rate,years;
+	cout<<"please input money year rete with space."<<endl;
 	cin>>money>>years>>rate;
 	while(money>0){
 		sum = money*pow((1+rate),years);
+		cout<<"The sum is: ";
 		cout<<sum<<endl;
 		cin>>money>>years>>rate;
 	}
