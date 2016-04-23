2ecad3b11bdeb6770b06c909a5d2a66dda57cf09
wangxiong <wxwangxiong@gmail.com>
2016-04-23 17:43:36 +0800

    lesson: 5-9-2

--- 163/lessson5a91.cpp
+++ 163/lessson5a91.cpp
@@ -1,19 +1,25 @@
 #include<iostream>
 using namespace std;
 
-double maxValue(double a, double b)
+double maxThree(double a, double b, double c)
 {
-	if(a>b)
-		return a;
-	else
-		return b;
+	double max;
+	max = a>b?a:b;
+	max = max>c?max:c;
+	return max;
 }
 
 int main()
 {
 	double a,b,c;
-	cin>>a>>b>>c;
-	double max = maxValue(maxValue(a,b),c);
-	cout<<max<<endl;
+	char yes;
+	do
+	{
+		cin>>a>>b>>c;
+		double max = maxThree(a,b,c);
+		cout<<max<<endl;
+		cout<<"continue(y/n)?"<<endl;
+		cin>>yes;
+	}while(yes=='Y'||yes=='y');
 	return 0;
 }
