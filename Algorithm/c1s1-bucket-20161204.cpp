26f541eb3b4394b37b3e8f7f94b365f0c1a5cd47
王雄 <wxwangxiong@gmail.com>
2016-12-04 14:21:39 +0800

    Add: Chapter 1,Section 1 桶排序

---
+++ Algorithm/c1s1-bucket-20161204.cpp
@@ -0,0 +1,29 @@
+/**
+ * Chapter 1,Section 1 桶排序
+ */
+#include <stdio.h>
+
+int main()
+{
+	int book[11],i,j,t;
+
+	for(i=0;i<=10;i++)
+		book[i] = 0;
+
+	for(i=1;i<=5;i++)
+	{
+		scanf("%d", &t);
+		book[t]++;
+	}
+
+	for(i=0;i<=10;i++)
+	{
+		for(j=1;j<=book[i];j++)
+			printf("%d ", i);
+	}
+
+	getchar();
+	getchar();
+
+	return 0;
+}
